d6c7d431fbd70016374ba77d7075d2b97425bfaa
Sebastian Brudzinski <seb.brudzinski@gmail.com>
2015-05-31 21:36:44 +0200

    Added application to test

---
+++ Blog/Blog.DAL.Tests/RepositoryTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Blog.DAL.Infrastructure;
+using Blog.DAL.Model;
+using Blog.DAL.Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using TDD.DbTestHelpers.Core;
+using TDD.DbTestHelpers.Yaml;
+
+namespace Blog.DAL.Tests
+{
+    [TestClass]
+    public class RepositoryTests : DbBaseTest<BlogFixtures>
+    {
+        [TestMethod]
+        public void GetAllPost_OnePostInDb_ReturnOnePost()
+        {
+            // arrange
+            //var context = new BlogContext();
+            //context.Database.CreateIfNotExists();
+            var repository = new BlogRepository();
+
+            //context.Posts.ToList().ForEach(x => context.Posts.Remove(x));
+            //context.Posts.Add(new Post{Author="test",Content="test, test, test..."});
+            //context.SaveChanges();
+
+            // act
+            var result = repository.GetAllPosts();
+            // assert
+            Assert.AreEqual(1, result.Count());
+        }
+
+        [TestMethod]
+        public void CreatePost_ShouldAddValidPost()
+        {
+            var repository = new BlogRepository();
+            repository.AddPost(new Post{ Author = "test2", Content = "test2" });
+
+            var result = repository.GetAllPosts();
+
+            //one initial and one added
+            Assert.AreEqual(2, result.Count());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CreatePost_ShouldThrowExceptionWhenValidationFails()
+        {
+            var repository = new BlogRepository();
+            repository.AddPost(new Post { Author = "", Content = "test2" });
+        }
+    }
+
+    public class BlogFixtures : YamlDbFixture<BlogContext, BlogFixturesModel>
+    {
+        public BlogFixtures()
+        {
+            SetYamlFiles("posts.yml");
+        }
+    }
+
+    public class BlogFixturesModel
+    {
+        public FixtureTable<Post> Posts { get; set; }
+    }
+}
---
+++ Blog/Blog.DAL/Repository/BlogRepository.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Blog.DAL.Infrastructure;
+using Blog.DAL.Model;
+using System;
+
+namespace Blog.DAL.Repository
+{
+    public class BlogRepository
+    {
+        private readonly BlogContext _context;
+
+        public BlogRepository()
+        {
+            _context = new BlogContext();
+        }
+
+        public IEnumerable<Post> GetAllPosts()
+        {
+            return _context.Posts;
+        }
+
+        public void AddPost(Post post)
+        {
+            if (post.Author == null || post.Author.Equals(""))
+                throw new ArgumentException ("Required property not set");
+            
+            _context.Posts.Add(post);
+            _context.SaveChanges();
+        }
+    }
+}
